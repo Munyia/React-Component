5bf3ab4dd29b7b925a2e1e0fefcbcb22b990dab6
Vivian <ndidiranjo@gmail.com>
2024-03-13 15:02:33 +0100

    adding components

---
+++ src/Price.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import product from './product'
+
+const Price = () => {
+  return (
+    <span>{product.price}</span>
+  )
+}
+
+export default Price
---
+++ src/product.js
@@ -0,0 +1,10 @@
+const product ={
+    name:"Jungle Boot",
+    price: "$25",
+    description: "Army Comouflage",
+    image: "https://s.alicdn.com/@sc04/kf/Ha9101a5fec7b42f48e95c96b973e5028r.jpg_300x300.jpg"
+}
+
+export default product
+
+ 
