486bf7bc74748cde9323919323843ef12ae154ab
Vivian <ndidiranjo@gmail.com>
2024-03-14 10:01:06 +0100

    adding comments and a readme file

--- README.md
+++ README.md
@@ -1,8 +1,32 @@
-# React + Vite
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+## Description
 
-Currently, two official plugins are available:
+This app displays a card with information about a product, including its name, description, and price. It also includes an image of the product.
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+## Technologies Used
+
+- React.js
+- Bootstrap
+- CSS
+
+## Installation
+
+1. Clone the repository.
+2. Run `npm install` to install dependencies.
+3. Run `npm start` to start the development server.
+4. Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+## Components
+
+- `Name`: Displays the name of the product.
+- `Description`: Displays the description of the product.
+- `Price`: Displays the price of the product.
+- `Image`: Displays the image of the product.
+
+## Usage
+
+This app can be used to showcase product information in a visually appealing way.
+
+## Credits
+
+Developed by Munyia.
--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,31 @@
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import Name from './Name';
-import Description from './Description';
-import Price from './Price';
-import Image from './Image';
+import './App.css'; // Importing custom CSS styles
+import 'bootstrap/dist/css/bootstrap.min.css'; // Importing Bootstrap CSS
+import Button from 'react-bootstrap/Button'; // Importing Button component from react-bootstrap library
+import Card from 'react-bootstrap/Card'; // Importing Card component from react-bootstrap library
+import Name from './Name'; // Importing Name component
+import Description from './Description'; // Importing Description component
+import Price from './Price'; // Importing Price component
+import Image from './Image'; // Importing Image component
 
 function App() {
-let name= "Munyia"
+  let name = "Munyia"; // Define name variable
+
   return (
-    <div id='comp'>
-    <Card style={{ width: '18rem' }}>
-      <Image/>
-      <Card.Body>
-        <Card.Title><Name/></Card.Title>
-        <Card.Text>
-          <Description/>
-        </Card.Text>
-        <Button variant="primary"><Price/></Button>
-      </Card.Body>
-    </Card>
-    <h1>Hello {name? name:"there" }</h1>
-    {
-      name && <img id='pet' src= 'https://previews.123rf.com/images/isselee/isselee2210/isselee221000210/192897090-tabby-striped-mixed-breed-cat-and-rottweiler-dog-looking-at-camera-isolated-on-white.jpg'></img>
-    }
+    <div id='comp'> {/* Main container with ID 'comp' */}
+      <Card style={{ width: '18rem' }}> {/* Bootstrap Card component */}
+        <Image /> {/* Image component */}
+        <Card.Body> {/* Card body */}
+          <Card.Title><Name /></Card.Title> {/* Display Name component */}
+          <Card.Text> {/* Card text */}
+            <Description /> {/* Display Description component */}
+          </Card.Text>
+          <Button variant="primary"><Price /></Button> {/* Display Price component inside Button */}
+        </Card.Body>
+      </Card>
+      <h1>Hello {name ? name : "there" }</h1> {/* Conditional rendering of greeting */}
+      {name && <img id='pet' src='https://previews.123rf.com/images/isselee/isselee2210/isselee221000210/192897090-tabby-striped-mixed-breed-cat-and-rottweiler-dog-looking-at-camera-isolated-on-white.jpg' alt="Pet" />} {/* Conditional rendering of image */}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
